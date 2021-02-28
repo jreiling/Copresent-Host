4290e20ae34003b0a52037aea1ce9e28bf093bb1
Jon Reiling <jreiling@mac.com>
2021-02-28 10:31:09 -0800

    Added README

--- Copresent/AppDelegate.swift
+++ Copresent/AppDelegate.swift
@@ -10,10 +10,15 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    var activity: NSObjectProtocol?
+
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        // Hack to keep the app responsive in the background.
+        activity = ProcessInfo().beginActivity(options: .userInitiatedAllowingIdleSystemSleep, reason: "Good Reason")
+
         // Insert code here to initialize your application
     }
 
